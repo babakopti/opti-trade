033f9ed0c190bcf5d516b964c3589ca0b8ce28a8
Babak Emami <babak.emami@gmail.com>
2019-09-06 12:51:51 -0700

    some chnages for new run

--- ode/odeBase.py
+++ ode/odeBase.py
@@ -103,7 +103,7 @@ class OdeBaseConst:
                                           y0       = self.bcVec, 
                                           t_span   = timeSpan,
                                           t_eval   = timeEval,
-                                          max_step = 1.0 * timeInc,
+                                          max_step = 10.0 * timeInc,
                                           method   = self.intgType, 
                                           rtol     = self.tol            )
         else:
@@ -111,7 +111,7 @@ class OdeBaseConst:
                                           y0       = self.bcVec, 
                                           t_span   = timeSpan,
                                           t_eval   = timeEval,
-                                          max_step = 1.0 * timeInc,
+                                          max_step = 10.0 * timeInc,
                                           method   = self.intgType, 
                                           rtol     = self.tol            )            
         sFlag    = res.success
