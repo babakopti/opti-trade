bee651aab43f43981f2362168dedd04ca5f99071
Babak Emami <babak.emami@gmail.com>
2019-09-27 13:58:32 -0700

    a minor change

--- scripts/cloud_setup.sh
+++ scripts/cloud_setup.sh
@@ -1,4 +1,8 @@
-sudo apt-get install git
+
+# Do these before running this script
+# sudo apt-get install git
+# git clone https://github.com/babakopti/opti-trade.git
+
 sudo apt-get install emacs
 sudo apt-get install python3-pip
 sudo pip3 install numpy pandas matplotlib scipy scikit-learn dill
