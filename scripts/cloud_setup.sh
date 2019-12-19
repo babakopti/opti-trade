a74e6bab291dc6083fb203afd4ff004bd0d3ae41
Babak Emami <optilive.developer@gmail.com>
2019-12-19 17:19:16 +0000

    adding schedule lib

--- scripts/cloud_setup.sh
+++ scripts/cloud_setup.sh
@@ -5,6 +5,7 @@
 sudo apt-get install emacs
 sudo apt-get install python3-pip
 sudo pip3 install numpy pandas matplotlib scipy scikit-learn dill
+sudo pip3 schedule
 
 git config --global status.showUntrackedFiles no
 git config --global user.email "optilive.developer@gmail.com"
