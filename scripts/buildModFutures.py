f00ab42f5130a6e1574b9ded36f8185fd45b341a
Babak Emami <optilive.developer@gmail.com>
2019-12-27 20:51:37 +0000

    Updating futures testing script

--- scripts/buildModFutures.py
+++ scripts/buildModFutures.py
@@ -19,32 +19,32 @@ from mod.mfdMod import MfdMod
 # Set some parameters and read data
 # ***********************************************************************
 
-dfFile  = 'data/dfFile_futures_daily.pkl'
+dfFile  = 'data/dfFile_futures.pkl'
 
-minTrnDate  = pd.to_datetime( '2010-01-01 09:00:00' )
-maxTrnDate  = pd.to_datetime( '2017-12-31 23:59:00' )
+minTrnDate  = pd.to_datetime( '2016-01-01 09:00:00' )
+maxTrnDate  = pd.to_datetime( '2019-06-30 23:59:00' )
 maxOosDate  = pd.to_datetime( '2019-12-25 23:59:00' )
 
 indexes = [ 'INDU', 'NDX', 'SPX', 'COMPQ', 'RUT',  'OEX',  
             'MID',  'SOX', 'RUI', 'RUA',   'TRAN', 'HGX',  
             'TYX'                      ] 
-#futures     = [ 'ES', 'NQ', 'US', 'YM', 'RTY', 'EMD', 'QM' ]
+futures     = [ 'ES', 'NQ', 'US', 'YM', 'EMD', 'QM' ]
 
-fuDf = pd.read_csv( 'data/Futures_kibot.txt', delimiter = '\t' )
+# fuDf = pd.read_csv( 'data/Futures_kibot.txt', delimiter = '\t' )
 
-fuDf[ 'Continuous' ] = fuDf.Description.apply( lambda x : 'CONTINUOUS' in x )
+# fuDf[ 'Continuous' ] = fuDf.Description.apply( lambda x : 'CONTINUOUS' in x )
 
-fuDf = fuDf[ fuDf.Continuous == True ]
+# fuDf = fuDf[ fuDf.Continuous == True ]
 
-fuDf = fuDf[ [ 'Base', 'StartDate', 'Description' ] ]
+# fuDf = fuDf[ [ 'Base', 'StartDate', 'Description' ] ]
 
-fuDf[ 'StartDate' ] = fuDf[ 'StartDate' ].apply( pd.to_datetime )
+# fuDf[ 'StartDate' ] = fuDf[ 'StartDate' ].apply( pd.to_datetime )
 
-fuDf.reset_index( drop = True, inplace = True )
+# fuDf.reset_index( drop = True, inplace = True )
 
-fuDf[ fuDf.StartDate <= pd.to_datetime( '2010-01-01' ) ].shape
+# fuDf[ fuDf.StartDate <= pd.to_datetime( '2010-01-01' ) ].shape
 
-futures = list( set( fuDf.Base ) - set( [ 'RTY', 'TN', 'BTC', 'SIR', 'SIL'  ] ) )
+# futures = list( set( fuDf.Base ) - set( [ 'RTY', 'TN', 'BTC', 'SIR', 'SIL'  ] ) )
 
 velNames    = futures + indexes
 
@@ -78,6 +78,6 @@ print( 'Success :', validFlag )
 
 mfdMod.save( modFileName )
 
-mfdMod.ecoMfd.pltResults( rType = 'trn', pType = pType )
-mfdMod.ecoMfd.pltResults( rType = 'oos', pType = pType )
+#mfdMod.ecoMfd.pltResults( rType = 'trn', pType = pType )
+#mfdMod.ecoMfd.pltResults( rType = 'oos', pType = pType )
 
