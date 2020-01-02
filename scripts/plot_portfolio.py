2e4f5fcc57b1cd87281342eb5170672d11eb2ad9
babakopti <optilive.developer@gmail.com>
2020-01-02 12:01:56 -0800

    Added inverse ETF hash to plot portfolio script

--- scripts/plot_portfolio.py
+++ scripts/plot_portfolio.py
@@ -17,11 +17,39 @@ import utl.utils as utl
 # Input
 # ***********************************************************************
 
-prtFile     = 'portfolios/portfolio_crash_macd.txt'
-dfFile      = 'data/dfFile_2005_2010.pkl'
+prtFile     = 'p_5sortETF_kibot.txt'
+dfFile      = 'data/dfFile_kibot_2017plus.pkl'
 base        = 'SPY'
 
 initTotVal  = 1000000.0
+
+invHash = {   'TQQQ' : 'SQQQ',
+              'SPY'  : 'SH',
+              'DDM'  : 'DXD',
+              'MVV'  : 'MZZ',
+              'UWM'  : 'TWM',
+              'SAA'  : 'SDD',
+              'UYM'  : 'SMN',
+              'UGE'  : 'SZK',
+              'UCC'  : 'SCC',
+              'FINU' : 'FINZ',
+              'RXL'  : 'RXD',
+              'UXI'  : 'SIJ',
+              'URE'  : 'SRS',
+              'ROM'  : 'REW',
+              'UJB'  : 'SJB',
+              'AGQ'  : 'ZSL',     
+              'DIG'  : 'DUG',
+              'USD'  : 'SSG',
+              'ERX'  : 'ERY',
+              'UYG'  : 'SKF',
+              'UCO'  : 'SCO',
+              'BOIL' : 'KOLD',
+              'UPW'  : 'SDP',
+              'UGL'  : 'GLL',
+              'BIB'  : 'BIS',
+              'UST'  : 'PST',
+              'UBT'  : 'TBT' }
 
 # ***********************************************************************
 # Read portfolio dates, assets
@@ -33,7 +61,13 @@ prtWtsHash = ast.literal_eval( open( prtFile, 'r' ).read() )
 # Get actual open prices
 # ***********************************************************************
 
-retDf = utl.calcPrtReturns( prtWtsHash, dfFile, initTotVal )
+retDf = utl.calcPrtReturns( prtWtsHash = prtWtsHash,
+                            dfFile     = dfFile,
+                            initTotVal = initTotVal,
+                            shortFlag  = False,
+                            invHash    = invHash,
+                            minDate    = None,
+                            maxDate    = None      )
 
 # ***********************************************************************
 # Plot
