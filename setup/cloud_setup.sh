fd2577f49ab099ca46601ac6bd0697618d3bbbf2
babakopti <optilive.developer@gmail.com>
2020-01-18 11:24:59 -0800

    Improving setup scripts

--- setup/cloud_setup.sh
+++ setup/cloud_setup.sh
@@ -2,28 +2,32 @@
 # sudo apt-get install git
 # git clone https://github.com/babakopti/opti-trade.git
 
-sudo apt-get install emacs
-sudo apt-get install python3-pip
-sudo pip3 install numpy pandas matplotlib scipy scikit-learn dill
-sudo pip3 install requests schedule google-cloud-storage
-
+# Setup git configs
 git config --global status.showUntrackedFiles no
 git config --global user.email "optilive.developer@gmail.com"
 git config --global user.name "Babak Emami"
 
+# Install some dependencies
 sudo apt-get install build-essential checkinstall
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
-
+# Build TA-Lib from source
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
 sudo ./configure
 sudo make
-
 sudo make install
-pip3 install ta-lib
+
+# Install emacs and pip3
+sudo apt-get install emacs
+sudo apt-get install python3-pip
+
+# Install python3 libs
+sudo pip3 install -r requirements.txt
+
+# Set the library path
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 # If a newer pythopn3 version was required
