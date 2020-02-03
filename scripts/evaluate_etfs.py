c2ac38b292f7c8914db983f3a4d734681fa1ade4
babakopti <optilive.developer@gmail.com>
2020-02-02 22:10:19 -0800

    Adding the new ETF evaluation script

--- scripts/evaluate_etfs.py
+++ scripts/evaluate_etfs.py
@@ -15,28 +15,65 @@ sys.path.append( os.path.abspath( '../' ) )
 
 import utl.utils as utl
 
+from dat.assets import OLD_ETF_HASH, POP_ETF_HASH, NEW_ETF_HASH
+
 # ***********************************************************************
 # Some definitions
 # ***********************************************************************
 
-nEvalDays = 90
+OUT_FILE  = 'new_etf_comparison.csv'
+NUM_EVAL_DAYS = 60
+ALL_ETF_FILE = 'data/All_ETFs_Intraday.txt'
+MIN_CNT = 100
 
-ETF_LIST  = 'data/All_ETFs_Intraday.txt'
-MIN_DAYS  = 10
-OUT_FILE  = 'etf_mad_comparison.csv'
+# ***********************************************************************
+# Generate description hash
+# ***********************************************************************
+
+eDf      = pd.read_csv( ALL_ETF_FILE, delimiter = '\t' )
+symbols  = list( eDf.Symbol )
+descs    = list( eDf.Description )
+descHash = {}
+
+for i in range( len( symbols ) ):
+
+    symbol = symbols[i]
+
+    descHash[ symbol ] = descs[i]
+    
+# ***********************************************************************
+# Set ETFs
+# ***********************************************************************
+
+ETFs = []
+
+searchStr = 'Palladium'
+
+for symbol in symbols:
+    if searchStr.lower() in descHash[ symbol ].lower():
+        ETFs.append( symbol )
+
+ETFs = list( set( NEW_ETF_HASH.values() ) )
 
 # ***********************************************************************
 # Utility functions
 # ***********************************************************************
 
-def getTrendMAD( df, symbol ):
+def getMeanMad( df, symbol ):
 
-    retDf = pd.DataFrame( { symbol: np.log( df[ symbol ] ).pct_change().dropna() } )
+    tmpDf = df.copy()
     
-    mad  = ( retDf - retDf.mean() ).abs().mean()
-    mad  = float( mad )
+    tmpFunc     = lambda x : pd.to_datetime( x ).date()
+    tmpDf[ 'tmp' ] = tmpDf.Date.apply( tmpFunc )
+    
+    tmpDf = tmpDf.groupby( [ 'tmp' ], as_index = False )[ [symbol] ].mean()
+    tmpDf = tmpDf.rename( columns = { 'tmp' : 'Date' } )
 
-    mean = retDf.mean()#df[ symbol ].pct_change().dropna().mean()
+    tmpVec = np.log( tmpDf[ symbol ] ).pct_change().dropna() 
+    
+    mad  = ( tmpVec - tmpVec.mean() ).abs().mean()
+    mad  = float( mad )
+    mean = tmpVec.mean()
     mean = float( mean )
     
     return mad, mean
@@ -45,15 +82,11 @@ def getTrendMAD( df, symbol ):
 # Evaluate
 # ***********************************************************************
 
-eDf   = pd.read_csv( ETF_LIST, delimiter = '\t' )
-ETFs  = list( eDf.Symbol )
-descs = list( eDf.Description )
-
-etfList  = []
-madList  = []
+etfList   = []
+descList  = []
+cntList   = []
+madList   = []
 meanList  = []
-descList = []
-dayList  = []
 
 logger = utl.getLogger( None, 1 )
 
@@ -61,60 +94,35 @@ for i in range( len( ETFs ) ):
 
     symbol = ETFs[i]
     
-    logger.info( 'Reading symbol %s...' % symbol )
-    
-    authFlag = utl.getKibotAuth()
-
-    assert authFlag, 'Authorization not successful!'
-
-    url  = 'http://api.kibot.com/?action=history'
-    url  = url + '&symbol=%s&interval=daily&period=%d&type=ETFs&regularsession=0' \
-        % ( symbol, nEvalDays )
-
-    resp = requests.get( url, timeout = 20 )
-
-    cols = [ 'Date',
-             'Open',
-             'High',
-             'Low',
-             'Close',
-             'Volume' ]
-    
-    df    = pd.read_csv( StringIO( resp.text ), names = cols )
-    df    = df.rename( columns = { 'Open' : symbol } )
-    df    = df[ [ 'Date', symbol ] ]
-    nRows = df.shape[0]
-        
-    logger.info( 'Got %d rows for %s!', nRows, symbol )
-
-    if nRows < MIN_DAYS:
-        logger.warning( 'Skipping %s: too few data, only %d days!',
-                        symbol,
-                        nRows )
+    try:
+        df = utl.getKibotData( etfs  = [ symbol ],
+                               nDays = NUM_EVAL_DAYS )
+    except Exception as e:
+        logger.warning( e )
         continue
 
     etfList.append( symbol )
+    descList.append( descHash[ symbol ] )
+    cntList.append( df.shape[0] )
     
-    descList.append( descs[i] )
-    
-    mad, mean  = getTrendMAD( df, symbol )
+    mad, mean  = getMeanMad( df, symbol )
 
     madList.append( mad )
-    
     meanList.append( mean )
 
-    dayList.append( nRows )
+outDf = pd.DataFrame( { 'asset': etfList,
+                        'mad'  : madList,
+                        'mean' : meanList,
+                        'desc' : descList,
+                        'cnt'  : cntList  } )
 
-outDf = pd.DataFrame( { 'Asset': etfList,
-                        'MAD'  : madList,
-                        'MEAN' : meanList,
-                        'Desc' : descList,
-                        'Days' : dayList  } )
+outDf[ 'abs_sharpe' ] = abs( outDf[ 'mean' ] ) / outDf[ 'mad' ]
 
-outDf[ 'RATIO' ] = outDf[ 'MEAN' ] / outDf[ 'MAD' ]
-
-outDf = outDf.sort_values( 'RATIO',
+outDf = outDf.sort_values( 'abs_sharpe',
                            ascending = False )
+
+outDf = outDf[ outDf.cnt >= MIN_CNT ]
+outDf = outDf.reset_index( drop = True )
 
 outDf.to_csv( OUT_FILE, index = False )
 
