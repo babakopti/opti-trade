7decd8f72a9c79555145915c70204ce4154bff5f
babakopti <optilive.developer@gmail.com>
2020-09-17 09:37:27 -0700

    Cleaning up a script

--- scripts/apply_ptc_to_portfolio.py
+++ scripts/apply_ptc_to_portfolio.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 sys.path.append( '..' )
 
-from ptc.ptc import PTClassifier
+import ptc.ptc as ptc
 
 # ***********************************************************************
 # Main input params
@@ -40,32 +40,107 @@ for dateStr in prtWtsHash:
 symbols = list( set( symbols ) )
 
 # ***********************************************************************
+# Some utility defines
+# ***********************************************************************
+
+def buildPTC( symList ):
+
+    for symbol in symList:
+
+        symFile = os.path.join( 'data',
+                                '%s.pkl' % symbol )
+        vixFile = os.path.join( 'data',
+                                'VIX.pkl' )            
+
+        ptcObj  = ptc.PTClassifier( symbol      = symbol,
+                                    symFile     = symFile,
+                                    vixFile     = vixFile,
+                                    ptThreshold = 1.0e-2,
+                                    nAvgDays    = 7,
+                                    nPTAvgDays  = None,
+                                    testRatio   = 0,
+                                    method      = 'bayes',
+                                    minVix      = minVix,
+                                    maxVix      = maxVix,
+                                    logFileName = None,                    
+                                    verbose     = 1          )
+
+        ptcObj.classify()
+
+        ptcFile = os.path.join( ptcDir,
+                                'ptc_' + symbol + '.pkl' )
+            
+        print( 'Saving the classifier to %s' % ptcFile )
+            
+        ptcObj.save( ptcFile )
+
+def adjustPTC( wtHash, snapDate ):
+
+    dayDf = pd.read_pickle( 'data/dfFile_2020.pkl' )        
+
+    dayDf[ 'Date' ] = dayDf.Date.astype( 'datetime64[ns]' )
+    
+    minDate = snapDate - \
+        pd.DateOffset( days = 3 * 7 )
+    
+    dayDf = dayDf[ ( dayDf.Date >= minDate ) &
+                   ( dayDf.Date <= snapDate ) ]
+    
+    dayDf[ 'Date' ] = dayDf.Date.\
+        apply( lambda x : x.strftime( '%Y-%m-%d' ) )
+    
+    dayDf = dayDf.groupby( 'Date', as_index = False ).mean()
+    
+    dayDf[ 'Date' ] = dayDf.Date.astype( 'datetime64[ns]' )
+        
+    dayDf = dayDf.sort_values( [ 'Date' ], ascending = True )
+
+    vixVal = list( dayDf.VIX )[-1]
+
+    if minVix is not None and vixVal < minVix:
+        return wtHash
+
+    if maxVix is not None and vixVal > maxVix:
+        return wtHash
+
+    for symbol in wtHash:
+            
+        dayDf[ 'vel' ]    = np.gradient( dayDf[ symbol ], 2 )
+        dayDf[ 'acl' ]    = np.gradient( dayDf[ 'vel' ], 2 )
+
+        tmpDate = snapDate - pd.DateOffset( days = 7 )
+        avgAcl = dayDf[ dayDf.Date >= tmpDate ].acl.mean()
+        symVal = list( dayDf.acl )[-1] - avgAcl
+            
+        ptcFile = os.path.join( ptcDir,
+                                'ptc_' + symbol + '.pkl' )
+            
+        obj = pickle.load( open( ptcFile, 'rb' ) )
+        
+        X = np.array( [ symVal ] ).reshape( ( 1, 1 ) )
+        
+        ptTag = obj.predict( X )[0]
+
+        if ptTag == ptc.PEAK:
+            print( 'A peak is detected for %s' % symbol )
+            
+            if wtHash[ symbol ] > 0:
+                print( 'Changing weight for %s from %0.2f to '
+                       '%0.2f as a peak was detected!' %
+                       ( symbol,
+                         wtHash[ symbol ],
+                         -wtHash[ symbol ] ) )
+                
+            wtHash[ symbol ] = -wtHash[ symbol ]
+
+    return wtHash
+
+# ***********************************************************************
 # Build PT classifiers if applicable
 # ***********************************************************************
 
 if ptcFlag:
-
-    for symbol in symbols:
-
-        symFile = '%s/%s.pkl' % ( datDir, symbol )
-        vixFile = '%s/VIX.pkl' % datDir
-
-        ptcObj = PTClassifier( symbol      = symbol,
-                               symFile     = symFile,
-                               vixFile     = vixFile,
-                               ptThreshold = 1.0e-2,
-                               nAvgDays    = 7,
-                               nPTAvgDays  = None,
-                               testRatio   = 0,
-                               method      = 'bayes',
-                               minVix      = minVix,
-                               maxVix      = maxVix,
-                               logFileName = None,                    
-                               verbose     = 1          )
-
-        ptcObj.classify()
-
-        ptcObj.save( '%s/ptc_%s.pkl' % ( ptcDir, symbol ) )
+    buildPTC( symbols )
 
 # ***********************************************************************
 # Apply ptc model
@@ -73,68 +148,15 @@ if ptcFlag:
 
 allDates = sorted( list( prtWtsHash.keys() ) )
 
-peakAdjCnt = 0
-peakAdjDates  = []
-
 for dateStr in allDates:
 
     print( 'Adjsuting snapDate %s' % dateStr )
     
     tmpHash  = prtWtsHash[ dateStr ]
     snapDate = pd.to_datetime( dateStr )
-    
-    df = pd.read_pickle( 'data/dfFile_2020.pkl' )
-    
-    df[ 'Date' ] = df.Date.astype( 'datetime64[ns]' )
-    
-    tmpDate = snapDate - pd.DateOffset( days = 21 )
-    
-    df = df[ ( df.Date >= tmpDate ) & ( df.Date <= snapDate ) ]
-    
-    df[ 'Date' ] = df.Date.apply( lambda x : x.strftime( '%Y-%m-%d' ) )
-    
-    dayDf = df.groupby( 'Date', as_index = False ).mean()
-    
-    dayDf[ 'Date' ] = dayDf.Date.astype( 'datetime64[ns]' )
-    
-    dayDf = dayDf.sort_values( [ 'Date' ], ascending = True )
-
-    vixVal = list( dayDf.VIX )[-1]
-    
-    if minVix is not None and vixVal < minVix:
-        continue
-
-    if maxVix is not None and vixVal > maxVix:
-        continue
-    
-    for symbol in tmpHash:
-        
-        dayDf[ 'vel' ] = np.gradient( dayDf[ symbol ], 2 )
-        dayDf[ 'acl' ] = np.gradient( dayDf[ 'vel' ], 2 )
-        
-        tmpDate = snapDate - pd.DateOffset( days = 7 )
-        avgAcl = dayDf[ dayDf.Date >= tmpDate ].acl.mean()
-        symVal = list( dayDf.acl )[-1] - avgAcl
-        
-        obj = pickle.load( open( '%s/ptc_%s.pkl' % ( ptcDir, symbol ), 'rb' ) )
-
-        X = np.array( [ symVal ] ).reshape( ( 1, 1 ) )
-        
-        ptTag = obj.predict( X )[0]
-        
-        if ptTag == 1:
-            print( 'Peak detected for %s at %s' % (symbol, dateStr))            
-            if tmpHash[ symbol ] > 0:
-                tmpHash[ symbol ] = -tmpHash[ symbol ]
-                peakAdjCnt += 1
-                peakAdjDates.append( dateStr )
-        # if ptTag == 2:
-        #     tmpHash[ symbol ] = abs( tmpHash[ symbol ] )
+    tmpHash  = adjustPTC( tmpHash, snapDate )
 
     prtWtsHash[ dateStr ] = tmpHash
-
-print( 'Changed weight sign %d times' % peakAdjCnt )
-print( 'Changed weights on dates: %s' % str(set(peakAdjDates)) )
 
 # ***********************************************************************
 # Write the adjusted portfolio
