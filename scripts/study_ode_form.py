577e4de97193e96ff0693d1eb2fdb3f01a2240dd
babakopti <optilive.developer@gmail.com>
2020-10-06 08:55:20 -0700

    Updated a script

--- scripts/study_ode_form.py
+++ scripts/study_ode_form.py
@@ -7,27 +7,27 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
 bcTime = 0.0
-endTime = 1.0
+endTime = 10.0
 nTimes = 1000
 bcVec = np.array([ 1.0, 1.0 ])
 Gamma = np.zeros( shape = ( 2, 2, 2 ), dtype = 'd' )
 
-Gamma[0][0][0] = 0.0
-Gamma[1][1][1] = 0.0
+Gamma[0][0][0] = 0.5
+Gamma[1][1][1] = 1.0
 
 Gamma[0][0][1] = -1.0
 Gamma[1][0][1] = 0.2
 
-Gamma[0][1][1] = 0.
-Gamma[1][0][0] = 0
+Gamma[0][1][1] = 0.5
+Gamma[1][0][0] = 0.1
 
 Gamma[0][1][0] = Gamma[0][0][1]
 Gamma[1][1][0] = Gamma[1][0][1]
 
 def fun( t, y ):
     srcTerm = np.zeros( shape = ( 2 ), dtype = 'd' )
-    if y[0] >= 1.5:
-        srcTerm[0] = -10.0 * ( y[0] - 2.5 )
+#    if y[0] >= 1.5:
+#        srcTerm[0] = -10.0 * ( y[0] - 2.5 )
 
     return -np.tensordot( Gamma,
                           np.tensordot( y, y, axes = 0 ),
@@ -36,20 +36,41 @@ def fun( t, y ):
 def jac( t, y ):
     return -2.0 * np.tensordot( Gamma, y, axes = ( (2), (0) ) )
 
-timeSpan = ( bcTime, endTime )        
-timeEval = np.linspace( bcTime, endTime, nTimes )
+def solve_geodesic( bcTime,
+                    endTime,
+                    nTimes,
+                    bcVec,
+                    Gamma  ):
 
-res = solve_ivp( fun      = fun, 
-                 #jac      = jac,
-                 y0       = bcVec, 
-                 t_span   = timeSpan,
-                 t_eval   = timeEval,
-                 method   = 'LSODA', 
-                 rtol     = 1.0e-6            )
+    timeSpan = ( bcTime, endTime )        
+    timeEval = np.linspace( bcTime, endTime, nTimes )
 
-print( 'Success:', res.success )
-y = res.y #np.flip( res.y, 1 )
+    res = solve_ivp( fun      = fun, 
+                     #jac      = jac,
+                     y0       = bcVec, 
+                     t_span   = timeSpan,
+                     t_eval   = timeEval,
+                     method   = 'LSODA', 
+                     rtol     = 1.0e-6     )
 
-plt.plot(y[0])
-plt.plot(y[1])
-plt.show()
+    print( 'Success:', res.success )
+    y = res.y #np.flip( res.y, 1 )
+
+    plt.plot(y[0], 'b' )
+    plt.plot(y[1], 'r' )
+    plt.legend([str(Gamma[0]), str(Gamma[1])])
+    plt.show()
+
+for itr in range( 20 ):
+
+    Gamma[0] = 0.5 * ( 2 * np.random.rand(2, 2) - 1 )
+    Gamma[1] = 0.5 * ( 2 * np.random.rand(2, 2) - 1 )
+
+    Gamma[0][1][0] = Gamma[0][0][1]
+    Gamma[1][1][0] = Gamma[1][0][1]
+
+    solve_geodesic( bcTime  = bcTime,
+                    endTime = endTime,
+                    nTimes  = nTimes,
+                    bcVec   = bcVec,
+                    Gamma   = Gamma    )    
