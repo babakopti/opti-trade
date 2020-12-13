e5911fe5babacc8b1b14236613ce1e08267a605a
babakopti <optilive.developer@gmail.com>
2020-12-12 21:38:03 -0800

    Added num_pers

--- scripts/opt_gnp.py
+++ scripts/opt_gnp.py
@@ -3,40 +3,29 @@ from apply_gain_preservation import getGnpPerf
 import json
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 DF_FILE = 'data/dfFile_2020.pkl'
 PRT_FILE = 'portfolios/nTrnDays_360_two_hours_ptc.json'
 
 prtWtsHash = json.load( open( PRT_FILE, 'r' ) )
 
-legends = []
-X = np.linspace(1.0, 2.0, 11 )
 perf_hash = {
     'std_coef': [],
     'pers_off': [],
+    'num_pers': [],
     'mean_std_ratio': [],
 }
 
-for pers_off in range(1, 21):
-    legends.append( 'pers_off %s' % str(pers_off) )
-    Y = []
-    for std_coef in X:
-        Y.append(
-            getGnpPerf(std_coef, pers_off, prtWtsHash, DF_FILE )
-        )
-        perf_hash[ 'std_coef' ].append( std_coef )
-        perf_hash[ 'pers_off' ].append( pers_off )
-        perf_hash [ 'mean_std_ratio' ].append( Y[-1] )
+for num_pers in [20, 40, 60, 80, 100, 120]:
+    for pers_off in range(1, 21):
+        for std_coef in np.linspace(1.0, 2.0, 11):
+            perf_hash[ 'std_coef' ].append( std_coef )
+            perf_hash[ 'pers_off' ].append( pers_off )
+            perf_hash[ 'num_pers' ] .append( num_pers )
+            perf_hash [ 'mean_std_ratio' ].append(
+                getGnpPerf(std_coef, pers_off, num_pers, prtWtsHash, DF_FILE )
+            )
         
-    plt.plot( X, Y )
-    
-plt.xlabel( 'std_coef' )
-plt.ylabel( 'Performance (Mean/Std ratio)' )
-plt.legend( legends )
-plt.title( 'Tuning of GNP' )
-plt.show()
-
 perf_df = pd.DataFrame( perf_hash )
 
 print( perf_df )
